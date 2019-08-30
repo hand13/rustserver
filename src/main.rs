4f1dc03b2c7d61c31c7a9a2c842dd7d38a6295c8
hand13 <1101738345@qq.com>
2019-08-30 20:53:25 +0800

    world

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,9 @@
-use std::net::TcpListener;
-use std::io::prelude::*;
-use std::net::TcpStream;
-use std::fs;
-pub mod request;
-use request::Request;
-use std::fmt;
+#![feature(proc_macro_hygiene,decl_macro)]
+#[macro_use] extern crate rocket;
+#[get("/hello/<name>")]
+fn hello(name : String) -> String {
+    format!("hello ,{}!",name.as_str())
+}
 fn main() {
-    let k = world(13);
-    let w = world(9);
-    work(&k);
-    work(&w);
-    /*
-    let listener = TcpListener::bind("localhost:7878").unwrap();
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        println!("connection establish");
-        handle_connection(stream);
-    }*/
-}
-fn handle_connection(mut stream :TcpStream) {
-    let mut buffer = [0;512];
-    stream.read(&mut buffer).unwrap();
-    //let request = String::from_utf8_lossy(&buffer[..]);
-    let content = fs::read_to_string("index.html").unwrap();
-    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}",content);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-}
-fn world(k:u32) -> Option<String>{
-    if k > 12 {
-        Option::None
-    }else {
-        Some(format!("the number is {}",k))
-    }
-}
-fn work(value:&Option<String>) {
-    match value {
-        None => println!("no value"),
-        Some(x) => println!("the value is {}",x)
-    }
+    rocket::ignite().mount("/",routes![hello]).launch();
 }
