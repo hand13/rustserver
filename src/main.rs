00013bae8d813dd835c3c1e34e8a48a32f5888bc
hand13 <1101738345@qq.com>
2019-09-13 21:49:04 +0800

    add more

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,36 @@ use chrono::prelude::*;
 use std::fs::File;
 use std::path::Path;
 use std::io::prelude::*;
+use rocket::request::Form;
+use rocket::Request;
+
+fn ifi(i:u32) -> u32 {
+    if i <=0 {
+        return 0;
+    }
+
+    if i == 1{
+        1
+    }else {
+        i * ifi(i-1)
+    }
+}
+
+#[derive(FromForm)]
+struct User {
+    name:String,
+    password:String
+}
+#[get("/user?<user..>")]
+fn user(user:Form<User>) -> String{
+    format!("name is {} password is {}",&user.name,&user.password)
+}
+
+#[get("/echo?<name>")]
+fn echo(name :String) ->String{
+    format!("echo {} ,number is {}",name,ifi(8))
+}
+
 
 #[get("/hello/<name>")]
 fn hello(name : String) -> String {
@@ -29,10 +59,15 @@ fn res() ->String{
     };
     format!("content  is {}",s)
 }
+#[catch(404)]
+fn not_found(req :&Request) -> String{
+    format!("sorry {} is not a valid url",req.uri())
+}
 fn main() {
     rocket::ignite()
+        .register(catchers![not_found])
         .mount("/",routes![hello])
         .mount("/lord",routes![get_time])
-        .mount("/lord",routes![res])
+        .mount("/lord",routes![res,echo,user])
         .launch();
 }
